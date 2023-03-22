de3e5c834e7a55e5fee6ec6bf6ced6186ede3a4e
Christian Smith <christiansmith@Christians-MBP.lan>
2023-03-22 15:22:00 -0400

    added inquirer

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -11,3 +11,19 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+inquirer.prompt ([
+    type: 'input',
+    
+])
+
+// GitHub username
+// email address
+// project name
+// write a short description
+// what kind of license should your project have- drop down list
+// what command should be ran to install dependencies
+// what command should be run to run test
+// what does the user need to know about using the repo
+// what does the user need to know about contributing to the repo
+// then generates
